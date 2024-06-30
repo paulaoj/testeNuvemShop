29981b0f5b2b1b655eecc56d5d7c0d1c1f777863
Paulo Henrique da Silva Júnior <132317175+paulaoj@users.noreply.github.com>
2024-06-30 19:25:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
     res.header("Access-Control-Allow-Headers", "Content-Type")
+    res.header("mode", "no-cors")
     app.use(cors())
     next()
 })
