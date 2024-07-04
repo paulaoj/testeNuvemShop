b1be207f34761dde0bb5048f7dce6f6892b8f4a9
Paulo Henrique da Silva Júnior <132317175+paulaoj@users.noreply.github.com>
2024-07-04 20:56:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const router = express.Router()
-var cors = require('cors');
+const cors = require('cors')
+
+app.use(cors({
+    origin: '*'
+}))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+    req.body('client_id', "12259")
+    req.body('client_secret', "37563a0c3347fd06eddc75e8d3607a29dc5c8e8a6000cee1")
+    req.body('grant_type', "authorization_code")
+    req.body('code', "1340d9e7472386e26eef189752d65c3901de4492")
+  });
+
+
 router.get("/", (req ,res)=>{
     res.sendFile(path.join(__dirname + "/index.html"))
 })
